b4ee2856cf0ea029caed9d122152d4f164ab1fbb
Tyler Long <tyler4long@gmail.com>
2020-07-02 20:35:03 -0700

    PubNub

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # RCV Client demo
 
 This project tries to build a RCV client from scratch.
+
+
+## Setup
+
+```
+cp .env.sample .env
+```
+
+Edit `.env` file to specify credentials.
+
+
+## Run
+
+```
+yarn test
+```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import RingCentral from 'ringcentral-extensible';
+import PubNubExtension from 'ringcentral-extensible/build/src/extensions/pubNub';
+// import waitFor from 'wait-for-async';
 
 const rc = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID!,
@@ -6,12 +8,22 @@ const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL!,
 });
 
+const pubNubExtension = new PubNubExtension();
+
 (async () => {
+  await rc.installExtension(pubNubExtension);
+
   await rc.authorize({
     username: process.env.RINGCENTRAL_USERNAME!,
     extension: process.env.RINGCENTRAL_EXTENSION!,
     password: process.env.RINGCENTRAL_PASSWORD!,
   });
+
+  // // PubNub
+  // pubNubExtension.subscribe(['/restapi/v1.0/conference'], event => {
+  //   console.log(JSON.stringify(event, null, 2));
+  // });
+  // await waitFor({interval: 10000});
 
   // // fetch extension info
   // const extInfo = await rc.restapi().account().extension().get();
