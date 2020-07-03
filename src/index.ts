cecccacf26d1722b121f01c967629ba57cc6ec7c
Tyler Long <tyler4long@gmail.com>
2020-07-02 19:51:59 -0700

    Get extension info

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log(process.env.RINGCENTRAL_EXTENSION);
+import RingCentral from 'ringcentral-extensible';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+  server: process.env.RINGCENTRAL_SERVER_URL!,
+});
+
+(async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION!,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+
+  const extInfo = await rc.restapi().account().extension().get();
+  console.log(JSON.stringify(extInfo, null, 2));
+
+  await rc.revoke();
+})();
