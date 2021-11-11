c119c864252efa8887b61c3fa0427b036a96dcc9
Tyler Long <tyler4long@gmail.com>
2021-11-11 12:21:53 -0800

    We don't really need the clientId because we have the token

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import RingCentral from '@rc-ex/core';
 
 const rc = new RingCentral({
-  clientId: process.env.RINGCENTRAL_CLIENT_ID,
   server: process.env.RINGCENTRAL_SERVER_URL,
 });
-
 rc.token = {
   access_token: process.env.RINGCENTRAL_ACCESS_TOKEN,
 };
