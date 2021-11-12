377083571543e3b19ff3556e6fe6ffee6d93e1ac
Tyler Long <tyler4long@gmail.com>
2021-11-11 16:21:09 -0800

    Send the first ws message

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import WebSocket from 'isomorphic-ws';
 
 import {Bridge, Meeting} from './types';
 
+const baseTime = Date.now();
+let req_seq = 0;
+
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
 });
@@ -41,15 +44,52 @@ rc.token = {
   const meeting = r.data as Meeting;
   console.log(JSON.stringify(meeting, null, 2));
 
+  const participant = meeting.participants[0];
+  const session = participant.sessions[0];
+
   const ws = new WebSocket(meeting.wsConnectionUrl);
   ws.addEventListener('open', (...args) => {
-    console.log('ws open', ...args);
+    console.log('ws open');
+    ws.send(
+      JSON.stringify(
+        {
+          req_src: 'webcli',
+          req_seq: req_seq++,
+          tx_ts: Date.now() - baseTime,
+          event: 'create_req',
+          body: {
+            max_remote_audio: 0,
+            max_remote_video: [
+              {
+                quality: 1,
+                slots: 0,
+              },
+            ],
+            conference_id: '',
+            sdp_semantics: 'plan-b',
+            token: session.token,
+            meta: {
+              meeting_id: bridge.shortId,
+              user_agent:
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+              x_user_agent:
+                'RCAppRCVWeb/21.3.33.0 (RingCentral; macos/10.15; rev.aaebe132)',
+            },
+          },
+          version: 1,
+          type: 'session',
+          id: session.id,
+        },
+        null,
+        2
+      )
+    );
   });
   ws.addEventListener('error', (...args) => {
     console.log('ws error', ...args);
   });
-  ws.addEventListener('message', (...args) => {
-    console.log('ws message', ...args);
+  ws.addEventListener('message', e => {
+    console.log('ws message', e.data);
   });
   ws.addEventListener('close', (...args) => {
     console.log('ws close', ...args);
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,37 @@
 export type Bridge = {
   id: string;
+  shortId: string;
 };
 
 export type Meeting = {
   id: string;
   wsConnectionUrl: string;
+  participants: Participant[];
+};
+
+export type Participant = {
+  id: string;
+  displayName: string;
+  sessions: Session[];
+};
+
+export type Session = {
+  id: string;
+  token: string;
+};
+
+export type CreateRespEvent = {
+  body: CreateRespEventBody;
+};
+
+export type CreateRespEventBody = {
+  sdp: string;
+  ice_servers: IceServer;
+  media_stat_interval: number;
+};
+
+export type IceServer = {
+  username: string;
+  credential: string;
+  urls: string[];
 };
